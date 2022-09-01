a636cea941155e56650ac2c0c0f950a3b26b5d69
L-Karimi <luciahkei101@gmail.com>
2022-09-01 21:58:05 +0300

    modified files

--- wallet/models.py
+++ wallet/models.py
@@ -20,13 +20,9 @@ class Customer(models.Model):
     nationality=models.CharField(max_length=20,null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/',null=True)
 
-
-
-
 class Currency(models.Model):
     amount=models.IntegerField()
     country_of_origin=models.CharField(max_length=24,null=True) 
-
 
 class Wallet(models.Model):
     currency =models.ForeignKey('Currency', on_delete=models.CASCADE, related_name ='Wallet_currency')
@@ -36,9 +32,6 @@ class Wallet(models.Model):
     date=models.DateTimeField(default=timezone.now)
     status=models.CharField(max_length=20,null=True)
     pin=models.TextField(max_length=6,null=True)
-
-
-
 
 class Account(models.Model):
     account_number=models.IntegerField()
@@ -62,8 +55,6 @@ class Transaction(models.Model):
     original_account=models.ForeignKey('Account', on_delete=models.CASCADE, related_name='Transaction_original_account')
     destination_account=models.ForeignKey('Account', on_delete=models.CASCADE, related_name='Transaction_destination_account')
 
-
-
 class Card(models.Model):
     date_Issued=models.DateTimeField(default=timezone.now)
     card_name=models.CharField(max_length=20,null=True)
@@ -84,8 +75,6 @@ class Card(models.Model):
     wallet=models.ForeignKey('Wallet', on_delete=models.CASCADE, related_name ='Card_wallet')
     account=models.ForeignKey('Account', on_delete=models.CASCADE, related_name ='Card_account')     
 
-
-
 # <class 'wallet.admin.ReceiptsAdmin'>: (admin.E108) The value of 'list_display[0]' refers to 'receipt_type', which is not a callable, an attribute of 'ReceiptsAdmin', or an attribute or method on 'wallet.Reward'.
 class ThirdParty(models.Model):
     account=models.ForeignKey('Account', on_delete=models.CASCADE, related_name ='ThirdParty_account')
@@ -93,7 +82,6 @@ class ThirdParty(models.Model):
     thirdparty_id=models.CharField(max_length=10,null=True)
     phone_Number=models.IntegerField()
     currency=models.ForeignKey('Currency', on_delete=models.CASCADE, related_name ='ThirdParty_currency')
-
 
 class Notifications(models.Model):
  notification_Id=models.CharField(max_length=25,null=True)
@@ -105,7 +93,6 @@ class Notifications(models.Model):
  date=models.DateTimeField(default=timezone.now)
  recipient=models.ForeignKey('Customer', on_delete=models.CASCADE, related_name ='Notifications_recipient')    
  
- 
 class Receipts(models.Model):
     receipt_type=models.CharField(max_length=25, null=True)
     receipt_date=models.DateTimeField(default=timezone.now)
@@ -114,8 +101,6 @@ class Receipts(models.Model):
     total_Amount=models.IntegerField(default=0)
     # transaction=models.ForeignKey('Transaction', on_delete=models.CASCADE, related_name ='Receipts_transaction')
     receipt_File=models.FileField(upload_to='wallet/')
-
- 
 
 class Loan(models.Model):
  loan_number=models.IntegerField()
@@ -128,7 +113,6 @@ class Loan(models.Model):
  due_date=models.DateTimeField(default=timezone.now)
  loan_balance=models.IntegerField()
  loan_term=models.IntegerField()
-
 
 
 class Reward(models.Model):  
