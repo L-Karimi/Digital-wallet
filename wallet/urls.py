e958037e9593aae4db8e680112962be479a170f9
L-Karimi <luciahkei101@gmail.com>
2022-10-05 18:44:04 +0300

    added urls

--- wallet/urls.py
+++ wallet/urls.py
@@ -24,7 +24,31 @@ urlpatterns=[
     path("thirdpartys/",views.list_thirdpartys,name="thirdpartys"),
     path("transactions/",views.list_transactions,name="transactions"),
     # path("customers",views.list_customers,name="customers"),
-    path("customers/init.id",customers_profile),
-    path("customers_edit/init.edit",edit_customer)
 
+    path('customers/<int:id>/', views.customers_profile, name="customers_profile"),
+    path('customers/edit/<int:id>/',views.edit_customer, name="edit_customer"),
+    
+    path('cards/<int:id>/', views.cards_profile, name="cards_profile"),
+    path('cards/edit/<int:id>/',views.edit_card, name="edit_card"),
+    
+    path('currencys/<int:id>/', views.currencys_profile, name="currencys_profile"),
+    path('currencys/edit/<int:id>/',views.edit_currency, name="edit_currency"),
+    
+    path('loan/<int:id>/', views.loan_profile, name="loan_profile"),
+    path('loan/edit/<int:id>/',views.edit_loan, name="edit_loan"),
+    
+    path('notifications/<int:id>/', views.notifications_profile, name="notifications_profile"),
+    path('notifications/edit/<int:id>/',views.edit_notifications, name="edit_notifications"),
+    
+    path('receipts/<int:id>/', views.receipts_profile, name="receipts_profile"),
+    path('receipts/edit/<int:id>/',views.edit_receipts, name="edit_receipts"),
+    
+    path('reward/<int:id>/', views.reward_profile, name="reward_profile"),
+    path('reward/edit/<int:id>/',views.edit_reward, name="edit_reward"),
+    
+    path('thirdparty/<int:id>/', views.thirdparty_profile, name="thirdparty_profile"),
+    path('thirdparty/edit/<int:id>/',views.edit_thirdparty, name="edit_thirdparty"),
+    
+    path('transaction/<int:id>/', views.transaction_profile, name="transaction_profile"),
+    path('transaction/edit/<int:id>/',views.edit_transaction, name="edit_transaction"),
     ]
